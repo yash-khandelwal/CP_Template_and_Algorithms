ee5605800a61fedc10e7ccd87bcf04db220bbccc
Kritagya Khandelwal <38128767+yash-khandelwal@users.noreply.github.com>
2019-09-04 16:46:55 +0530

    Add files via upload
    common template to use in cp questions

---
+++ common_template/common_template.cpp
@@ -0,0 +1,32 @@
+/*
+
+    Author: Kritagya
+    Date:   04-09-2019
+
+*/
+#include<bits/stdc++.h>
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl
+#define f(i,n) for(int i=0;i<(n);i++)
+#define fa(i,a,b) for(int i=(a);i<(b);i++)
+#define far(i,a,b) for(int i=(a);i>(b);i--)
+#define vect_i vector <int>
+#define pb push_back
+#define pf push_front
+#define T int t; cin>>t; while(t--)
+#define ll long long int
+#define ull unsigned long long int
+#define ld long double
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define mod = 1000000007
+using namespace std;
+long long int m=1000000007;
+long long int ncr[1001][1001];
+/////////////////////////////////////////////////////////////////////
+int main()
+{
+    FIO;
+    T{
+
+    }
+    return 0;
+}
